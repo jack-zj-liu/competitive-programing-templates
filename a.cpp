71d27e67eb96ed654700961084c6ebb4d7d680fa
leeds9z <76793746+leeds9z@users.noreply.github.com>
2020-12-31 14:06:19 -0800

    Add files via upload
    codeforces template

---
+++ a.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+void solve()
+{
+	int n;
+	cin >> n;
+	int a[n];
+	
+	//for(int i = 0; i<n; i++) cin >> a[i];
+
+}
+
+int main()
+{
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	int t;
+	cin >> t;
+	while(t--) solve();
+	return 0;
+}
